16e70da026346bf3f853a3337a8a14e04baa7c97
Gabriel <gabriel02ramos@gmail.com>
2023-06-11 18:16:26 -0300

    Add pt1 do dinamico

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 async function Profile() {
-    const url = 'http://raw.githubusercontent.com/Dionos-s/Portifolio/master/profile.json';
+    const url = 'https://raw.githubusercontent.com/Dionos-s/Portifolio/master/profile.json';
     const fetchin = await fetch(url)
     return await fetchin.json()
 }
